2f8cadcd117a2562ab2938d7c64ea2102a3fe03e
yangcar <93997620+yangcar@users.noreply.github.com>
2021-11-09 10:49:23 -0500

    print in python

---
+++ main.py
@@ -0,0 +1,14 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+string_var = "clase de electiva de programacion"
+integer_var = 123
+float_var = 123.24
+print(string_var)
+print(type(string_var))
+
+print(integer_var)
+print(type(integer_var))
+
+print(float_var)
+print(type(float_var))
